70513170497b5f64f43f60c37fde4017209ff86b
Evgeniy Sinev <es@payneteasy.com>
2018-04-22 21:55:02 +0300

    [inpas] Added USB support

--- src/main/java/com/payneteasy/reader/i18n/impl/ReaderI18nServiceImpl.java
+++ src/main/java/com/payneteasy/reader/i18n/impl/ReaderI18nServiceImpl.java
@@ -33,33 +33,33 @@ public class ReaderI18nServiceImpl implements IReaderI18nService {
             return translator.get("error.unknown");
         }
         switch(aCardError.getType()) {
-            case PARSE_PACKET_ERROR: return translator.get("error.parse_packet_error");
-            case PARSE_TRACK_ERROR: return translator.get("error.parse_track_error");
-            case EMPTY_CARD_INFO: return translator.get("error.empty_card_info");
-            case UNKNOWN_CARD_TYPE: return translator.get("error.unknown_card_type");
-            case CARD_NOT_VALID: return translator.get("error.card_not_valid");
-            case USER_CANCELLED: return translator.get("error.user_cancelled");
-            case SKIP: return translator.get("error.skip");
-            case INVALID_DATA_IN_COMMAND_APDU: return translator.get("error.invalid_data_in_command_apdu");
-            case TERMINAL_NOR_READY: return translator.get("error.terminal_nor_ready");
-            case NO_SMART_CARD_IN_SLOT: return translator.get("error.no_smart_card_in_slot");
-            case INVALID_CARD: return translator.get("error.invalid_card");
-            case INVALID_CARD_NO_MSR_FALLBACK_ALLOWED: return translator.get("error.invalid_card_no_msr_fallback_allowed");
-            case TRANSACTION_ALREADY_IN_PROGRESS: return translator.get("error.transaction_already_in_progress");
-            case DATA_MISSING_FROM_COMMAND_APDU: return translator.get("error.data_missing_from_command_apdu");
-            case UNSUPPORTED_CARD: return translator.get("error.unsupported_card");
-            case MISSING_FILE: return translator.get("error.missing_file");
-            case ICC_READ_ERROR: return translator.get("error.icc_read_error");
-            case INVALID_ISSUE_PUBLIC_KEY: return translator.get("error.invalid_issue_public_key");
-            case CONFIGURATION_ERROR: return translator.get("error.configuration_error");
-            case ENCRYPTION_INITIALISE_ERROR: return translator.get("error.encryption_initialise_error");
-            case CONTACTLESS_TRANSACTION_TIMED_OUT: return translator.get("error.contactless_transaction_timed_out");
+            case PARSE_PACKET_ERROR:                        return translator.get("error.parse_packet_error");
+            case PARSE_TRACK_ERROR:                         return translator.get("error.parse_track_error");
+            case EMPTY_CARD_INFO:                           return translator.get("error.empty_card_info");
+            case UNKNOWN_CARD_TYPE:                         return translator.get("error.unknown_card_type");
+            case CARD_NOT_VALID:                            return translator.get("error.card_not_valid");
+            case USER_CANCELLED:                            return translator.get("error.user_cancelled");
+            case SKIP:                                      return translator.get("error.skip");
+            case INVALID_DATA_IN_COMMAND_APDU:              return translator.get("error.invalid_data_in_command_apdu");
+            case TERMINAL_NOR_READY:                        return translator.get("error.terminal_nor_ready");
+            case NO_SMART_CARD_IN_SLOT:                     return translator.get("error.no_smart_card_in_slot");
+            case INVALID_CARD:                              return translator.get("error.invalid_card");
+            case INVALID_CARD_NO_MSR_FALLBACK_ALLOWED:      return translator.get("error.invalid_card_no_msr_fallback_allowed");
+            case TRANSACTION_ALREADY_IN_PROGRESS:           return translator.get("error.transaction_already_in_progress");
+            case DATA_MISSING_FROM_COMMAND_APDU:            return translator.get("error.data_missing_from_command_apdu");
+            case UNSUPPORTED_CARD:                          return translator.get("error.unsupported_card");
+            case MISSING_FILE:                              return translator.get("error.missing_file");
+            case ICC_READ_ERROR:                            return translator.get("error.icc_read_error");
+            case INVALID_ISSUE_PUBLIC_KEY:                  return translator.get("error.invalid_issue_public_key");
+            case CONFIGURATION_ERROR:                       return translator.get("error.configuration_error");
+            case ENCRYPTION_INITIALISE_ERROR:               return translator.get("error.encryption_initialise_error");
+            case CONTACTLESS_TRANSACTION_TIMED_OUT:         return translator.get("error.contactless_transaction_timed_out");
             case CONTACTLESS_NO_CONDITIONS_USE_ICEE_OR_MSR: return translator.get("error.contactless_no_conditions_use_icee_or_msr");
-            case CONTACTLESS_NOT_POSSIBLE_USE_ICEE_OR_MSR: return translator.get("error.contactless_not_possible_use_icee_or_msr");
-            case CONTACTLESS_ASK_CHIP_USE_ICC: return translator.get("error.contactless_ask_chip_use_icc");
-            case CONTACTLESS_HARDWARE_ERROR: return translator.get("error.contactless_hardware_error");
-            case CONTACTLESS_REQUIRED_EMV_FINAL_ADVICE: return translator.get("error.contactless_required_emv_final_advice");
-            case UNKNOWN: return translator.get("error.unknown");
+            case CONTACTLESS_NOT_POSSIBLE_USE_ICEE_OR_MSR:  return translator.get("error.contactless_not_possible_use_icee_or_msr");
+            case CONTACTLESS_ASK_CHIP_USE_ICC:              return translator.get("error.contactless_ask_chip_use_icc");
+            case CONTACTLESS_HARDWARE_ERROR:                return translator.get("error.contactless_hardware_error");
+            case CONTACTLESS_REQUIRED_EMV_FINAL_ADVICE:     return translator.get("error.contactless_required_emv_final_advice");
+            case UNKNOWN:                                   return translator.get("error.unknown");
         }
         return aCardError + "";
     }
@@ -75,17 +75,22 @@ public class ReaderI18nServiceImpl implements IReaderI18nService {
         }
         Translator translator = createTranslator(aLocale);
         switch(aCardReaderProblem) {
-            case BLUETOOTH_ERROR_CONNECTING: return translator.get("problem.bluetooth_error_connecting");
-            case BLUETOOTH_ADAPTER_NOT_FOUND: return translator.get("problem.bluetooth_adapter_not_found");
-            case BLUETOOTH_NO_PERMISSIONS: return translator.get("problem.bluetooth_no_permissions");
-            case BLUETOOTH_DISABLED: return translator.get("problem.bluetooth_disabled");
-            case BLUETOOTH_READER_NOT_PAIRED: return translator.get("problem.bluetooth_reader_not_paired");
-            case BLUETOOTH_PAIRING_REQUEST: return translator.get("problem.bluetooth_pairing_request");
-            case NETWORK_ERROR_CONNECTING: return translator.get("problem.network_error_connecting");
-            case AUDIO_AUDIORECORD_INIT_ERROR: return translator.get("problem.audio_audiorecord_init_error");
-            case AUDIO_AUDIORECORD_NOT_READY: return translator.get("problem.audio_audiorecord_not_ready");
-            case AUDIO_POWER_INIT_ERROR: return translator.get("problem.audio_power_init_error");
-            case AUDIO_READER_NOT_SUPPORTED: return translator.get("problem.audio_reader_not_supported");
+            case BLUETOOTH_ERROR_CONNECTING:    return translator.get("problem.bluetooth_error_connecting");
+            case BLUETOOTH_ADAPTER_NOT_FOUND:   return translator.get("problem.bluetooth_adapter_not_found");
+            case BLUETOOTH_NO_PERMISSIONS:      return translator.get("problem.bluetooth_no_permissions");
+            case BLUETOOTH_DISABLED:            return translator.get("problem.bluetooth_disabled");
+            case BLUETOOTH_READER_NOT_PAIRED:   return translator.get("problem.bluetooth_reader_not_paired");
+            case BLUETOOTH_PAIRING_REQUEST:     return translator.get("problem.bluetooth_pairing_request");
+            case NETWORK_ERROR_CONNECTING:      return translator.get("problem.network_error_connecting");
+            case AUDIO_AUDIORECORD_INIT_ERROR:  return translator.get("problem.audio_audiorecord_init_error");
+            case AUDIO_AUDIORECORD_NOT_READY:   return translator.get("problem.audio_audiorecord_not_ready");
+            case AUDIO_POWER_INIT_ERROR:        return translator.get("problem.audio_power_init_error");
+            case AUDIO_READER_NOT_SUPPORTED:    return translator.get("problem.audio_reader_not_supported");
+            case USB_CONNECTION_ERROR:          return translator.get("problem.usb_reader_connection_error");
+            case USB_DEVICE_NOT_FOUND:          return translator.get("problem.usb_reader_device_not_found");
+            case USB_ENDOINTS_NOT_FOUND:        return translator.get("problem.usb_reader_endpoints_not_found");
+            case USB_DEVICE_PERMISSION_DENIED:  return translator.get("problem.usb_reader_permission_denied");
+
         }
         return aCardReaderProblem + "";
     }
